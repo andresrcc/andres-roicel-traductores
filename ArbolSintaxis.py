cf1afbf553ed3102e75beb87265e4858850c9461
andresrcc <andrescordova87@gmail.com>
2012-06-07 23:55:43 -0430

    3ra Actualizacion con los Cambios de Roicel. Ya imprime el arbol para op binarias

--- ArbolSintaxis.py
+++ ArbolSintaxis.py
@@ -1,156 +1,168 @@
-# Autores: Roicel Laya    04-37161
-#          Andres Cordova 05-38050
+# Autores: Roicel Laya 04-37161
+# Andres Cordova 05-38050
 #
 #----------------------------------------------------------
 
 #Arbol Sintactico Abstracto
 
 class Arbol:
-    "Representa al Arbol de Sintaxis Abstracta"
-    def __init__(self,izq,op,der,tipo):
+    """Representa al Arbol de Sintaxis Abstracta"""
+    def __init__(self,izq,valor,der,tipo):
         self.tipo = tipo
-        self.izq  = izq
-        self.der  = der
-        self.op   = op
+        self.izq = izq
+        self.der = der
+        self.valor = valor
 
-    def show(self):
-        print "Prueba:\n"
-        return "Tipo: " + self.tipo + "\n" + \
-            "Nodo Izq: " + self.izq + "\n" + \
-            "Nodo Op: " + self.op + "\n" + \
-            "Nodo Der: " + self.der
-
+    def show(self, profundidad):
+        indentar = ' '*profundidad
+        print indentar + str(self.tipo)
+        if self.izq:
+            self.izq.show(profundidad + 2)
+        if self.der:
+            self.der.show(profundidad + 2)
 
 class Expresion(Arbol):
-    "Representa las expresiones del lenguaje"
+    """Representa las expresiones del lenguaje"""
     pass
 
 class Instruccion(Arbol):
-    "Representa las instrucciones del lenguaje"
+    """Representa las instrucciones del lenguaje"""
     pass
 
-#Operacion Binaria (Nodo)
-class OpBinaria(Expresion):
-    "Representa las Operaciones Binarias del lenguajes"
+#Expresion Binaria (Nodo)
+class ExpBinaria(Expresion):
+    """Representa las Operaciones Binarias del lenguajes"""
 
-    def __init__(self,izq,op,der):
-        self.izq   = izq
-        self.op    = op
-        self.der   = der
-
-    def show(self,profundidad):
-        """ Imprime el valor del nodo OpBinaria"""
-
-        if self.op == '+':
+    def __init__(self,izq,valor,der):
+        self.izq = izq
+        self.der = der
+        self.valor = valor
+        if self.valor == '+':
             self.operacion = 'Suma'
             self.tipo = 'integer'
-        elif self.op == '-':
+        elif self.valor == '-':
             self.operacion = 'Resta'
             self.tipo = 'integer'
-        elif self.op == '*':
+        elif self.valor == '*':
             self.operacion = 'Multiplicacion'
             self.tipo = 'integer'
-        elif self.op == '/':
+        elif self.valor == '/':
             self.operacion = 'Division'
-            self.tipo = 'integer' 
-        elif self.op == '%':
+            self.tipo = 'integer'
+        elif self.valor == '%':
             self.operacion = 'Modulo'
-            self.tipo = 'integer' 
-        elif self.op == '<':
+            self.tipo = 'integer'
+        elif self.valor == '<':
             self.operacion = 'Menor Estricto que'
             self.tipo = 'boolean'
-        elif self.op == '>':
+        elif self.valor == '>':
             self.operacion = 'Mayor Estricto que'
             self.tipo = 'boolean'
-        elif self.op == '=':
+        elif self.valor == '=':
             self.operacion = 'Igual'
             self.tipo = 'boolean'
-        elif self.op == '/=':
+        elif self.valor == '/=':
             self.operacion = 'Desigual'
             self.tipo = 'boolean'
-        elif self.op == '<=':
+        elif self.valor == '<=':
             self.operacion = 'Menor o Igual que'
             self.tipo = 'boolean'
-        elif self.op == '<=':
+        elif self.valor == '<=':
             self.operacion = 'Mayor o Igual que'
             self.tipo = 'boolean'
-        elif self.op == '/\\':
+        elif self.valor == '/\\':
             self.operacion = 'Conjuncion'
             self.tipo = 'boolean'
-        elif self.op == '\\/':
+        elif self.valor == '\\/':
             self.operacion = 'Disyuncion'
             self.tipo = 'boolean'
 
+    def show(self, profundidad):
         indentar = ' '*profundidad
-
-        return indentar + "-Operacion: " + self.operacion + "\n" \
-            + indentar + "-tipo: " + self.tipo + "\n" \
-            + indentar + "-operando izquierdo:" + self.izq.show(profundidad+1)\
-            +"\n" + indentar + "-operando derecho: " + self.der.show(profundidad+1)+"\n"
-        
-
+        print indentar + '- operacion: ' + str(self.operacion)
+        print indentar + '- tipo: ' + str(self.tipo)
+        if self.izq:
+            print indentar + '- operando izquierdo: '
+            self.izq.show(profundidad + 2)
+        if self.der:
+            print indentar + '- operando derecho: '
+            self.der.show(profundidad + 2)
 
 #Hojas
 class Num(Expresion):
-    "Clase Num: Representa los numeros del lenguaje"
+    """Clase Num: Representa los numeros del lenguaje"""
     def __init__(self,valor):
+        self.tipo = None
+        self.izq = None
+        self.der = None
         self.valor = valor
+
     def show(self, profundidad):
-        """ Imprime el valor del nodo Num"""
-        identar = ' '*profundidad
-        return 'INT_LITERAL ' + str(self.valor)
+        indentar = ' '*profundidad
+        print indentar + 'INT_LITERAL ' + str(self.valor)
 
 class Ident(Expresion):
-    "Representa los identificadores del lenguaje"
+    """Representa los identificadores del lenguaje"""
     def __init__(self,valor):
+        self.tipo = None
+        self.izq = None
+        self.der = None
         self.valor = valor
 
     def show(self, profundidad):
-        """ Imprime el Valor del nodo Ident"""
         indentar = ' '*profundidad
-        return indentar + 'VAR ' + str(self.valor)
-        
+        print indentar + 'VAR ' + str(self.valor)
 
 class BoolN(Expresion):
-    "Clase BoolN: Representa los Valores Booleanos del Lenguaje"
+    """Clase BoolN: Representa los Valores Booleanos del Lenguaje"""
     def __init__(self,valor):
+        self.tipo = None
+        self.izq = None
+        self.der = None
         self.valor = valor
 
-    def show(self,profundidad):
-        """ Imprime el valor del nodo BoolN"""
+    def show(self, profundidad):
         indentar = ' '*profundidad
-        return indentar + 'BOOL ' + self.valor
+        print indentar + 'BOOL_LITERAL ' + str(self.valor)
 
+class UResta(Expresion):
+    def __init__(self,valor):
+        self.tipo = None
+        self.izq = None
+        self.der = None
+        self.valor = valor
+
+    def show(self, profundidad):
+        indentar = ' '*profundidad
+        print indentar + 'INT_LITERAL ' + str(self.valor)
 
 #Area de Prueba del Modulo
-if __name__ == "__main__":    
+if __name__ == "__main__":
     print "Probando los metodos show de las clases"
 
 #Probando los identificadores
     print "Clase Ident:\n"
-    s = raw_input('Escriba el nombre de un identificador:')
+    s = raw_input('Escriba el nombre de un identificador: ')
     print "\nEl metodo show imprime: "
-    f = Ident(s).show(1)
-    print f
+    Ident(s).show(0)
 
-#Probando los numeros    
+#Probando los numeros
     print "Clase Num:\n"
-    s = raw_input('Escriba un numero:')
+    s = raw_input('Escriba un numero: ')
     print "\nEl metodo show imprime: "
-    g = Num(s).show(1)
-    print g
+    Num(s).show(0)
 
 #Probando las operaciones binarias:
 
     print "Clase OpBinaria:\n"
-    s = raw_input('Escriba una operacion binaria (USE ESPACIOS ENTRE LOS SIMBOLOS):')
+    s = raw_input('Escriba una operacion binaria (USE ESPACIOS ENTRE LOS SIMBOLOS): ')
     print "\nEl metodo show imprime: "
     (a,b,c) = s.split()
-    h = OpBinaria(a,b,c).show(1)
-    print h
+    h = ExpBinaria(a,b,c)
+    print h.show(0)
 
     print "Clase BoolN:\n"
     s = raw_input('Escriba un valor booleano: ')
-    print "\nEl metodo show imprime: "    
-    i = BoolN(s).show(1)
+    print "\nEl metodo show imprime: "
+    i = BoolN(s).show(0)
     print i
--- SintAsgard.py
+++ SintAsgard.py
@@ -1,17 +1,11 @@
-# Autores: Roicel Laya    04-37161
-#          Andres Cordova 05-38050
+# Autores: Roicel Laya 04-37161
+# Andres Cordova 05-38050
 #
 #---------------------------------------------------------
 
-# COMENTARIOS IMPORTANTES
-#
-# Se deben sustituir todas las operaciones debajo
-# de cada produccion por las operaciones referentes
-# a las clases de ArbolSintaxis.
-# 
 
 #Imports
-from ply import yacc as yacc 
+from ply import yacc as yacc
 import Lexer
 import ArbolSintaxis as ast
 
@@ -20,50 +14,27 @@ tokens = Lexer.tokens
 
 #Precedencias
 precedence = (
-    ('right', 'TkAsignacion'),
+    ('nonassoc', 'TkAsignacion'),
     ('left', 'TkDisyuncion'),
     ('left', 'TkConjuncion'),
-    ('left', 'TkIgual', 'TkDesigual'),
-    ('left', 'TkMenor', 'TkMayor', 
+    ('nonassoc', 'TkIgual', 'TkDesigual'),
+    ('nonassoc', 'TkMenor', 'TkMayor',
      'TkMenorIgual', 'TkMayorIgual'),
     ('left', 'TkSuma', 'TkResta'),
     ('left', 'TkMult', 'TkDiv', 'TkMod'),
+    ('right', 'UTkResta'),
 )
 
 #Gramatica Inicial
 #Instrucciones
 
-#Secuenciacion
-def p_instruccion(p):
-    'instruccion : expresion TkPuntoYComa expresion' 
-
-#Condicional
-def p_instruccionIf(p):
-    'instruccion : TkIf expresion TkThen instruccion'
-    if p[2] == true:
-        p[0] = p[4]
-
-def p_instruccionBegin(p):
-    '''instruccion   : TkBegin instruccion TkEnd
-                     | TkUsing expresion TkBegin instruccion TkEnd'''
-    if p[1] == 'begin':
-        p[0] = [1]
-#Como se incluye la lista de declaraciones de using?
-    elif p[1] == 'using':
-        p[0] = p[4]
-       
-def p_instruccionOfT(p):
-    'instruccion : expresion TkOfType TkNum'
-    p[0] = int(p[1])
-
-
 #Expresion puede ser operacion binaria
-def p_expresion(p):
-    '''expresion : expresion TkSuma  expresion
+def p_expresion_opbinaria(p):
+    '''expresion : expresion TkSuma expresion
                  | expresion TkResta expresion
-                 | expresion TkMult  expresion
-                 | expresion TkDiv   expresion
-                 | expresion TkMod   expresion
+                 | expresion TkMult expresion
+                 | expresion TkDiv expresion
+                 | expresion TkMod expresion
                  | expresion TkMenor expresion
                  | expresion TkMayor expresion
                  | expresion TkIgual expresion
@@ -72,7 +43,12 @@ def p_expresion(p):
                  | expresion TkMayorIgual expresion
                  | expresion TkConjuncion expresion
                  | expresion TkDisyuncion expresion'''
-    p[0] = ast.OpBinaria(p[1],p[2],p[3])
+    p[0] = ast.ExpBinaria(p[1],p[2],p[3])
+
+#Expresion unaria del simbolo menos
+def p_expresion_umenos(p):
+    'expresion : TkResta expresion %prec UTkResta'
+    p[0] = ast.UResta(-p[2])
 
 #Expresion puede ser numero, booleano, o variable
 def p_expresion_TkNum(p):
@@ -83,55 +59,12 @@ def p_expresion_TkIdent(p):
     'expresion : TkIdent'
     p[0] = ast.Ident(p[1])
 
-def p_expresion_TkBoolean(p):
-    'expresion : TkBoolean'
-    p[0] = p[1]
-
 # Booleano puede ser verdadero o falso
 
-def p_true(p):
-    'expresion : TkTrue'
-    p[0] = True
-
-def p_false(p):
-    'expresion : TkFalse'
-    p[0] = False
-
-#Asignacion
-def p_TkIdent_AsigTkNum(p):
-    'expresion : TkIdent TkAsignacion TkNum'
-    p[0] = p[1]
-
-def p_TkIdent_TkT(p):
-    'expresion : TkIdent TkAsignacion TkTrue'
-    p[0] = p[1]
-
-def p_TkIdent_TkF(p):
-    'expresion : TkIdent TkAsignacion TkFalse'
-
-#Operadores Unarios
-#Negacion
-def p_TkNeg_TkTrue(p):
-    'expresion : TkNegacion TkTrue'
-    p[0] = False
-
-def p_TkNeg_TkFalse(p):
-    'expresion : TkNegacion TkFalse'
-    p[0] = True
-
-#Negativo 
-def p_TkNeg_TkNum(p):
-    'expresion : TkResta TkNum'
-    p[0] = - p[2]
-
-#Parentizacion
-def p_expresion_Paren(p):
-    'expresion : TkParAbre expresion TkParCierra'
-    p[0] = p[2]
-
-
-#Secuenciacion
-
+def p_expresion_TkBoolean(p):
+    '''expresion : TkTrue
+                 | TkFalse'''
+    p[0] = ast.BoolN(p[1])
 
 # Errores de Sintaxis
 def p_error(p):
@@ -142,11 +75,10 @@ parser = yacc.yacc()
 
 while True:
     try:
-        s = raw_input('AsGArD >')
+        s = raw_input('AsGArD > ')
     except EOFError:
         break
-    if not s: 
+    if not s:
         continue
     result = parser.parse(s)
-    print result
-
+    result.show(0)
